e316b8c2c5b8d82296e531d5cfe3f92daf802fd8
subroh0508 <subroh.0508@gmail.com>
2019-05-24 23:28:31 +0900

    [edit]Edit dependency string to Dep object on settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,8 +11,8 @@ pluginManagement {
     resolutionStrategy {
         eachPlugin {
             when (requested.id.id) {
-                "kotlin2js", "kotlin-dce-js" -> useModule("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.31")
-                "org.jetbrains.kotlin.frontend" -> useModule("org.jetbrains.kotlin:kotlin-frontend-plugin:0.0.45")
+                "kotlin2js", "kotlin-dce-js" -> useModule(Dep.Plugin.kotlinGradle)
+                "org.jetbrains.kotlin.frontend" -> useModule(Dep.Plugin.kotlinFrontend)
             }
         }
     }
