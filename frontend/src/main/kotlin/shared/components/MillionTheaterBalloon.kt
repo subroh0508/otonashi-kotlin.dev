1a3b1a9d947701a9ba1fade71bbad436670dfdc8
subroh0508 <subroh.0508@gmail.com>
2019-06-22 00:36:51 +0900

    [add]Add takagi avatar

--- frontend/src/main/kotlin/shared/components/MillionTheaterBalloon.kt
+++ frontend/src/main/kotlin/shared/components/MillionTheaterBalloon.kt
@@ -1,15 +1,19 @@
 package shared.components
 
-import kotlinx.html.DIV
-import kotlinx.html.SECTION
-import kotlinx.html.div
-import kotlinx.html.span
+import kotlinx.html.*
 
 fun SECTION.millionBalloon(
     name: String,
+    withTakagi: Boolean = false,
     block: DIV.() -> Unit
 ) {
     div("balloon-container") {
+        if (withTakagi) {
+            div("takagi") {
+                img(src = "takagi.png")
+            }
+        }
+
         div("million-balloon") {
             span("character-name") { +name }
             div("script", block = block)
--- frontend/src/main/kotlin/shared/components/revealjs/RevealJsComponent.kt
+++ frontend/src/main/kotlin/shared/components/revealjs/RevealJsComponent.kt
@@ -14,7 +14,53 @@ import kotlin.collections.set
 class RevealJsComponent : RComponent<RProps, RState>() {
     override fun componentDidMount() {
         revealBuilder("reveal-frame") {
-            section {  }
+            section {
+                attributes["data-background-video"] = "/clip/01.mp4"
+                attributes["data-background-size"] = "contain"
+
+                millionBalloon("　") {
+                    p("fragment script-content") {
+                        attributes["data-autoslide"] = "1000"
+
+                        +"ある日..."
+                    }
+                    p("fragment script-content") {
+
+                    }
+                }
+            }
+            section {
+                attributes["data-background-video"] = "/clip/02.mp4"
+                attributes["data-background-size"] = "contain"
+
+                millionBalloon("小鳥") {
+                    p("fragment script-content") {
+                        attributes["data-autoslide"] = "1000"
+
+                        +"おはようございます、社長！"
+                    }
+                    p("fragment script-content") {
+
+                    }
+                }
+            }
+
+            section {
+                attributes["data-background-image"] = "/clip/02.png"
+                attributes["data-background-size"] = "contain"
+
+                millionBalloon("高木社長", withTakagi = true) {
+                    p("fragment script-content") {
+                        attributes["data-autoslide"] = "1000"
+
+                        +"おはよう！　音無君！"
+                    }
+                    p("fragment script-content") {
+                        +"そう言えば今日は、新しいコーポレートサイトの入稿日だったね？"
+                    }
+                }
+            }
+
             section {
                 attributes["data-background-video"] = "/clip/04.mp4"
                 attributes["data-background-size"] = "contain"
